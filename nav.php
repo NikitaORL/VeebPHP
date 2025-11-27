f87ef2010980698b840b41f87b7d0e5f327ee615
NikitaORL <nikita.orlenko234@gmail.com>
2025-11-27 14:04:59 +0200

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,22 @@
+
+<div id="main">
+    <div id="content">
+        <nav class="menu">
+            <ul>
+                <li><a href="?link=avaleht.php">Avaleht</a></li>
+                <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
+                <li><a href="?link=kalkulaator.php">Kalkulaator</a></li>
+                <li><a href="?link=MinuRuhm.php">Minu Ruhm</a></li>
+                <li><a href="https://nikitaorlenko24.thkit.ee/">Vana index</a></li>
+
+                <li><a href="#">Funktsioonid</a>
+                    <ul class="dropdown">
+                        <li><a href="?link=ajafunctsioonid.php">Ajafunktsioonid</a></li>
+                        <li><a href="?link=textFunktsioonid.php">Tekst Funktsioonid</a></li>
+                        <li><a href="?link=Matemaatika_functsioonid.php">Matemaatika</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
